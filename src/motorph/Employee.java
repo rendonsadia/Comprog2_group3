eb92794acaa7eb72b4ad2c6b1d778834501211a5
HP-VICTUS <HP-VICTUS@LAPTOP-PU9A5PFL>
2025-05-18 16:09:06 +0800

    This is our project for comprog 2

---
+++ src/motorph/Employee.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package motorph;
+
+/**
+ *
+ * @author HP-VICTUS
+ */
+public class Employee {
+    int employeeID;
+    String name;
+    String position;
+    String birthday;
+    String contactInfo;
+    String department;
+
+    public void getEmployeeInfo() {
+        System.out.println("ID: " + employeeID + ", Name: " + name + ", Department: " + department);
+    }
+}
+
+

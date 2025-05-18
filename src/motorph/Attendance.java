0b9e92d8e538068e00050e3b64b38f3a9d7fb3d5
HP-VICTUS <HP-VICTUS@LAPTOP-PU9A5PFL>
2025-05-18 16:16:09 +0800

    This is our project for comprog 2

---
+++ src/motorph/Attendance.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package motorph;
+
+/**
+ *
+ * @author HP-VICTUS
+ */
+public class Attendance {
+     int localDate;
+    String timeIn;
+    String timeOut;
+
+    public double calculateWorkedHours() {
+        // Dummy logic for example
+        return 8.0;
+    }
+}
+

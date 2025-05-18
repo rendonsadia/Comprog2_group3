237da012d19f5fd1f2a1e02129810b2a29aaecd4
HP-VICTUS <HP-VICTUS@LAPTOP-PU9A5PFL>
2025-05-18 16:14:49 +0800

    This is our project for comprog 2

---
+++ src/motorph/Admin.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package motorph;
+
+/**
+ *
+ * @author HP-VICTUS
+ */
+public class Admin {
+     String adminName;
+    String adminPassword;
+
+    public String createEmployeeInfo() {
+        return "Employee created by Admin.";
+    }
+
+    public String createUser() {
+        return "User created.";
+    }
+
+    public String createPassword() {
+        return "Password generated.";
+    }
+}
+}
